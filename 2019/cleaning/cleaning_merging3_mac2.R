49c5561d0515896a4b1fa8c5ceb359a4d314cc99
robchavez <robert.s.chavez@gmail.com>
2019-04-11 21:28:02 -0700

    code dump

---
+++ 2019/cleaning/cleaning_merging3_mac2.R
@@ -0,0 +1,252 @@
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(reshape2)
+
+# load season data
+
+rawdf <- read_csv("~/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/all_regseason_cleaned_1996-2019.csv")
+#rawdf <- read_csv("C:/Users/rober//Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/all_regseason_cleaned_1996-2019.csv")
+
+nonadf <- rawdf %>% select(-SchoolAdvancedPace, 
+                 -SchoolAdvancedORtg, 
+                 -HomeW, 
+                 -HomeL, 
+                 -AwayW, 
+                 -AwayL, 
+                 -SchoolAdvancedORB_percent, 
+                 -SchoolAdvancedSTL_percent, 
+                 -`School TotalsORB`,
+                 -`School TotalsMP`,
+                 -`School TotalsPF`,
+                 -`School TotalsTOV`,
+                 -SchoolAdvancedTOV_percent)
+
+
+# load tournament data
+#tourn <- read_csv("C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/all_tournament_wins_1996-2018.csv")
+tourn <- read_csv("~/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/all_tournament_wins_1996-2018.csv")
+
+# rename teams
+new_team1 <- tourn$team1
+new_team1 <- ifelse(new_team1 == "BYU"	,	"Brigham Young", new_team1)
+new_team1 <- ifelse(new_team1 == "Central Connecticut"	,	"Central Connecticut State", new_team1)
+new_team1 <- ifelse(new_team1 == "Detroit"	,	"Detroit Mercy", new_team1)
+new_team1 <- ifelse(new_team1 == "ETSU"	,	"East Tennessee State", new_team1)
+new_team1 <- ifelse(new_team1 == "LIU-Brooklyn"	,	"Long Island University", new_team1)
+new_team1 <- ifelse(new_team1 == "LSU"	,	"Louisiana State", new_team1)
+new_team1 <- ifelse(new_team1 == "Ole Miss"	,	"Mississippi", new_team1)
+new_team1 <- ifelse(new_team1 == "Penn"	,	"Pennsylvania", new_team1)
+new_team1 <- ifelse(new_team1 == "Pitt"	,	"Pittsburgh", new_team1)
+new_team1 <- ifelse(new_team1 == "SMU"	,	"Southern Methodist", new_team1)
+new_team1 <- ifelse(new_team1 == "Southern Miss"	,	"Southern Mississippi", new_team1)
+new_team1 <- ifelse(new_team1 == "St. Joseph's"	,	"Saint Joseph's", new_team1)
+new_team1 <- ifelse(new_team1 == "St. Peter's"	,	"Saint Peter's", new_team1)
+new_team1 <- ifelse(new_team1 == "UCF"	,	"Central Florida", new_team1)
+new_team1 <- ifelse(new_team1 == "UConn"	,	"Connecticut", new_team1)
+new_team1 <- ifelse(new_team1 == "UCSB"	,	"UC-Santa Barbara", new_team1)
+new_team1 <- ifelse(new_team1 == "UIC"	,	"Illinois-Chicago", new_team1)
+new_team1 <- ifelse(new_team1 == "UMass"	,	"Massachusetts", new_team1)
+new_team1 <- ifelse(new_team1 == "UMBC"	,	"Maryland-Baltimore County", new_team1)
+new_team1 <- ifelse(new_team1 == "UNC"	,	"North Carolina", new_team1)
+new_team1 <- ifelse(new_team1 == "UNLV"	,	"Nevada-Las Vegas", new_team1)
+new_team1 <- ifelse(new_team1 == "USC"	,	"Southern California", new_team1)
+new_team1 <- ifelse(new_team1 == "UTEP"	,	"Texas-El Paso", new_team1)
+new_team1 <- ifelse(new_team1 == "UTSA"	,	"Texas-San Antonio", new_team1)
+new_team1 <- ifelse(new_team1 == "VCU"	,	"Virginia Commonwealth", new_team1)
+new_team1 <- ifelse(new_team1 == "California"	,	"University of California", new_team1)
+new_team1 <- ifelse(new_team1 == "UNC Greensboro"	,	"North Carolina-Greensboro", new_team1)
+new_team1 <- ifelse(new_team1 == "Saint Mary's"	,	"Saint Mary's (CA)", new_team1)
+new_team1 <- ifelse(new_team1 == "TCU",	"Texas Christian", new_team1)
+new_team1 <- ifelse(new_team1 == "UNC Wilmington"	,	"North Carolina-Wilmington", new_team1)
+new_team1 <- ifelse(new_team1 == "NC State", "North Carolina State", new_team1)
+new_team1 <- ifelse(new_team1 == "UNC Asheville",	"North Carolina-Asheville", new_team1)
+new_team1 <- ifelse(new_team1 == "Little Rock"	,	"Arkansas-Little Rock", new_team1)
+
+
+new_team2 <- tourn$team2
+new_team2 <- ifelse(new_team2 == "BYU"	,	"Brigham Young", new_team2)
+new_team2 <- ifelse(new_team2 == "Central Connecticut"	,	"Central Connecticut State", new_team2)
+new_team2 <- ifelse(new_team2 == "Detroit"	,	"Detroit Mercy", new_team2)
+new_team2 <- ifelse(new_team2 == "ETSU"	,	"East Tennessee State", new_team2)
+new_team2 <- ifelse(new_team2 == "LIU-Brooklyn"	,	"Long Island University", new_team2)
+new_team2 <- ifelse(new_team2 == "LSU"	,	"Louisiana State", new_team2)
+new_team2 <- ifelse(new_team2 == "Ole Miss"	,	"Mississippi", new_team2)
+new_team2 <- ifelse(new_team2 == "Penn"	,	"Pennsylvania", new_team2)
+new_team2 <- ifelse(new_team2 == "Pitt"	,	"Pittsburgh", new_team2)
+new_team2 <- ifelse(new_team2 == "SMU"	,	"Southern Methodist", new_team2)
+new_team2 <- ifelse(new_team2 == "Southern Miss"	,	"Southern Mississippi", new_team2)
+new_team2 <- ifelse(new_team2 == "St. Joseph's"	,	"Saint Joseph's", new_team2)
+new_team2 <- ifelse(new_team2 == "St. Peter's"	,	"Saint Peter's", new_team2)
+new_team2 <- ifelse(new_team2 == "UCF"	,	"Central Florida", new_team2)
+new_team2 <- ifelse(new_team2 == "UConn"	,	"Connecticut", new_team2)
+new_team2 <- ifelse(new_team2 == "UCSB"	,	"UC-Santa Barbara", new_team2)
+new_team2 <- ifelse(new_team2 == "UIC"	,	"Illinois-Chicago", new_team2)
+new_team2 <- ifelse(new_team2 == "UMass"	,	"Massachusetts", new_team2)
+new_team2 <- ifelse(new_team2 == "UMBC"	,	"Maryland-Baltimore County", new_team2)
+new_team2 <- ifelse(new_team2 == "UNC"	,	"North Carolina", new_team2)
+new_team2 <- ifelse(new_team2 == "UNLV"	,	"Nevada-Las Vegas", new_team2)
+new_team2 <- ifelse(new_team2 == "USC"	,	"Southern California", new_team2)
+new_team2 <- ifelse(new_team2 == "UTEP"	,	"Texas-El Paso", new_team2)
+new_team2 <- ifelse(new_team2 == "UTSA"	,	"Texas-San Antonio", new_team2)
+new_team2 <- ifelse(new_team2 == "VCU"	,	"Virginia Commonwealth", new_team2)
+new_team2 <- ifelse(new_team2 == "California"	,	"University of California", new_team2)
+new_team2 <- ifelse(new_team2 == "UNC Greensboro"	,	"North Carolina-Greensboro", new_team2)
+new_team2 <- ifelse(new_team2 == "Saint Mary's"	,	"Saint Mary's (CA)", new_team2)
+new_team2 <- ifelse(new_team2 == "TCU",	"Texas Christian", new_team2)
+new_team2 <- ifelse(new_team2 == "UNC Wilmington"	,	"North Carolina-Wilmington", new_team2)
+new_team2 <- ifelse(new_team2 == "NC State", "North Carolina State", new_team2)
+new_team2 <- ifelse(new_team2 == "UNC Asheville",	"North Carolina-Asheville", new_team2)
+new_team2 <- ifelse(new_team2 == "Little Rock"	,	"Arkansas-Little Rock", new_team2)
+
+
+new_winner <- tourn$winner
+new_winner <- ifelse(new_winner == "BYU"	,	"Brigham Young", new_winner)
+new_winner <- ifelse(new_winner == "Central Connecticut"	,	"Central Connecticut State", new_winner)
+new_winner <- ifelse(new_winner == "Detroit"	,	"Detroit Mercy", new_winner)
+new_winner <- ifelse(new_winner == "ETSU"	,	"East Tennessee State", new_winner)
+new_winner <- ifelse(new_winner == "LIU-Brooklyn"	,	"Long Island University", new_winner)
+new_winner <- ifelse(new_winner == "LSU"	,	"Louisiana State", new_winner)
+new_winner <- ifelse(new_winner == "Ole Miss"	,	"Mississippi", new_winner)
+new_winner <- ifelse(new_winner == "Penn"	,	"Pennsylvania", new_winner)
+new_winner <- ifelse(new_winner == "Pitt"	,	"Pittsburgh", new_winner)
+new_winner <- ifelse(new_winner == "SMU"	,	"Southern Methodist", new_winner)
+new_winner <- ifelse(new_winner == "Southern Miss"	,	"Southern Mississippi", new_winner)
+new_winner <- ifelse(new_winner == "St. Joseph's"	,	"Saint Joseph's", new_winner)
+new_winner <- ifelse(new_winner == "St. Peter's"	,	"Saint Peter's", new_winner)
+new_winner <- ifelse(new_winner == "UCF"	,	"Central Florida", new_winner)
+new_winner <- ifelse(new_winner == "UConn"	,	"Connecticut", new_winner)
+new_winner <- ifelse(new_winner == "UCSB"	,	"UC-Santa Barbara", new_winner)
+new_winner <- ifelse(new_winner == "UIC"	,	"Illinois-Chicago", new_winner)
+new_winner <- ifelse(new_winner == "UMass"	,	"Massachusetts", new_winner)
+new_winner <- ifelse(new_winner == "UMBC"	,	"Maryland-Baltimore County", new_winner)
+new_winner <- ifelse(new_winner == "UNC"	,	"North Carolina", new_winner)
+new_winner <- ifelse(new_winner == "UNLV"	,	"Nevada-Las Vegas", new_winner)
+new_winner <- ifelse(new_winner == "USC"	,	"Southern California", new_winner)
+new_winner <- ifelse(new_winner == "UTEP"	,	"Texas-El Paso", new_winner)
+new_winner <- ifelse(new_winner == "UTSA"	,	"Texas-San Antonio", new_winner)
+new_winner <- ifelse(new_winner == "VCU"	,	"Virginia Commonwealth", new_winner)
+new_winner <- ifelse(new_winner == "California"	,	"University of California", new_winner)
+new_winner <- ifelse(new_winner == "UNC Greensboro"	,	"North Carolina-Greensboro", new_winner)
+new_winner <- ifelse(new_winner == "Saint Mary's"	,	"Saint Mary's (CA)", new_winner)
+new_winner <- ifelse(new_winner == "TCU",	"Texas Christian", new_winner)
+new_winner <- ifelse(new_winner == "UNC Wilmington"	,	"North Carolina-Wilmington", new_winner)
+new_winner <- ifelse(new_winner == "NC State", "North Carolina State", new_winner)
+new_winner <- ifelse(new_winner == "UNC Asheville",	"North Carolina-Asheville", new_winner)
+new_winner <- ifelse(new_winner == "Little Rock"	,	"Arkansas-Little Rock", new_winner)
+
+
+tourn$team1 <- new_team1
+tourn$team2  <- new_team2
+tourn$winner <- new_winner
+tourn <- tourn %>% select(team1, team2,winner,season,team1_seed,team2_seed )
+
+#write_csv(tourn, "C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/renamed_all_tournament_wins_1996-2018.csv")
+write_csv(tourn, "~/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/renamed_all_tournament_wins_1996-2018.csv")
+
+
+
+# merge seed info with season data
+t1df <- data.frame(School = tourn$team1, seed = tourn$team1_seed, season = tourn$season)
+t2df <- data.frame(School = tourn$team2, seed = tourn$team2_seed, season = tourn$season)
+
+tourn_melt <- rbind(t1df,t2df)
+tourn_melt_uni <- unique(tourn_melt)
+tourn_melt_uni$School <- as.character(tourn_melt_uni$School)
+
+all_School <- data.frame(School = nonadf$School, season = nonadf$season)
+all_School$School <- as.character(all_School$School)
+
+school_seeds <- left_join(all_School,tourn_melt_uni)
+
+ 
+nonadf$seed <- school_seeds$seed
+
+
+#write_csv(nonadf, "C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/seeded_all_regseason_cleaned_1996-2018.csv")
+#write_csv(nonadf, "C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/Processed_data/joined_data.csv")
+
+write_csv(nonadf, "~/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/seeded_all_regseason_cleaned_1996-2018.csv")
+write_csv(nonadf, "~/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/Processed_data/joined_data.csv")
+
+
+
+#--------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------
+
+
+
+# load data
+seeddf <- read_csv("~/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/seeded_all_regseason_cleaned_1996-2018.csv")
+#seeddf <- read_csv("C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/seeded_all_regseason_cleaned_1996-2018.csv")
+
+
+#seeddf$School <- ifelse(seeddf$School == "University of California", "California", seeddf$School)
+#seeddf$School <- ifelse(seeddf$School == "North Carolina-Greensboro", "UNC Greensboro"	, seeddf$School)
+#seeddf$School <- ifelse(seeddf$School == 	"Saint Mary's (CA)", "Saint Mary's"	, seeddf$School)
+#seeddf$School <- ifelse(seeddf$School == 	"Texas Christian", "TCU"	, seeddf$School)
+#seeddf$School <- ifelse(seeddf$School == 	"North Carolina-Wilmington", "UNC Wilmington"	, seeddf$School)
+#seeddf$School <- ifelse(seeddf$School == 	"North Carolina State", "NC State"	, seeddf$School)
+#seeddf$School <- ifelse(seeddf$School == 	"North Carolina-Asheville", "UNC Asheville"	, seeddf$School)
+#seeddf$School <- ifelse(seeddf$School == 	"Arkansas-Little Rock", "Little Rock",seeddf$School)
+
+Radford
+
+#seeddf <- read_csv("C:/Users/rober//Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/seeded_all_regseason_cleaned_1996-2018.csv")
+
+#tourney <- read_csv("C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/renamed_all_tournament_wins_1996-2018.csv")
+tourney <- read_csv("~/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/renamed_all_tournament_wins_1996-2018.csv")
+
+
+# filter to tournament teams
+tourney_teams <- seeddf %>% filter(is.na(seed)==FALSE)
+
+
+
+# tournament munging
+tourney$wteam <- tourney$winner
+tourney$lteam <- ifelse(tourney$team1 == tourney$winner, tourney$team2, tourney$team1 )
+
+tourney_results <- tourney %>% select(season, wteam, lteam)
+
+seeddf <- as.data.frame(seeddf)
+tourney_results <- as.data.frame(tourney_results)
+tourney_teams <- as.data.frame(tourney_teams)
+
+#tourney_results$gameID <- 1:length(tourney_results$wteam) 
+
+
+both_vars_w <- data.frame()
+
+for(i in 1:length(tourney_results$wteam)){
+  x <- filter(seeddf, School == tourney_results[i,2], season == tourney_results[i,1])
+  y <- filter(seeddf, School == tourney_results[i,3], season == tourney_results[i,1]) 
+  new <- data.frame(x,y)
+  both_vars_w <- rbind(both_vars_w,new)
+}
+
+both_vars_data_w <- data.frame(tourney_results, both_vars_w)   
+
+
+both_vars_l <- data.frame()
+
+for(i in 1:length(tourney_results$wteam)){
+  x <- filter(seeddf, School == tourney_results[i,2], season == tourney_results[i,1])
+  y <- filter(seeddf, School == tourney_results[i,3], season == tourney_results[i,1]) 
+  new <- data.frame(y,x)
+  both_vars_l <- rbind(both_vars_l,new)
+}
+
+both_vars_data_l <- data.frame(tourney_results, both_vars_l)   
+
+
+both_vars_data_all <- rbind(both_vars_data_w, both_vars_data_l)
+
+both_vars_data_all$variable <- ifelse(both_vars_data_all$wteam == both_vars_data_all$School, "wteam", "lteam") 
+
+
+
+# Save difference data for modeling
+#write_csv(both_vars_data_all,"C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit//March_madness_model_2019/Processed_data/both_model_data.csv")
+write_csv(both_vars_data_all,"~/Google Drive/Code_scripts/R/forthefuckofit//March_madness_model_2019/Processed_data/both_model_data.csv")
+
---
+++ 2019/modeling/2019_modeling.R
@@ -0,0 +1,227 @@
+library(dplyr)
+library(caret)
+library(readr)
+library(PerformanceAnalytics)
+
+
+# Model setup and training -----------------------------------------
+
+both_data <- read_csv("C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/Processed_data/both_model_data.csv")
+both_data <- read_csv("~/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/Processed_data/both_model_data.csv")
+
+
+df2016 <- both_data %>% filter(season ==2016) %>% select(-season, -School,  -wteam, -season.1, -season.2, -lteam,  -School.1)
+df2017 <- both_data %>% filter(season ==2017) %>% select(-season, -School,  -wteam, -season.1, -season.2, -lteam,  -School.1)
+
+Dataset_test <- both_data %>% filter(season != 2017, season != 2018, season != 2019, is.na(seed)==FALSE, is.na(seed.1)==FALSE) %>% select(-season, -School,  -wteam, -season.1, -season.2, - lteam,  -School.1)
+Dataset_test <- both_data %>% 
+  filter(season != 2017, season != 2018, season != 2019, is.na(seed)==FALSE, is.na(seed.1)==FALSE) %>% 
+  select(-season, -School,  -wteam, -season.1, -season.2, - lteam,  -School.1)
+
+
+#Fit a Random Forest model
+tunegrid <- expand.grid(.mtry=c(2,15))
+model <- train(variable~.,Dataset_test,
+               method='gbm', preProcess = 'scale',
+               #tuneGrid=tunegrid,
+               trControl=trainControl(
+                 method='cv',number=10,
+                 verbose = TRUE,
+                 classProbs = TRUE))
+model$results
+
+
+
+
+
+# Predictions 2017 --------------------------------------------------------
+
+test_data <- read_csv("~/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/Processed_data/joined_data.csv")
+test_data <- read_csv("C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/Processed_data/joined_data.csv")
+test_data <- test_data %>% filter(season == "2017") %>% filter(is.na(seed)==F)
+
+#teams_2017 <- read.csv("C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/teams_2017_new.csv")
+#teams_2017 <- teams_2017 %>% filter(season2017 == 1) 
+
+
+wins <- read_csv("~/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/renamed_all_tournament_wins_1996-2018.csv")
+teams2017 <- wins %>% filter(season==2017) 
+teams2017 <- c(teams2017$team1,teams2017$team2)
+teams2017 <- data.frame(School = unique(teams2017)) 
+teams2017$School <- as.character(teams2017$School)
+teams2017 <- teams2017 %>% arrange(School)
+
+meh <- inner_join(test_data,teams2017) 
+test_data <- meh  %>% filter(is.na(seed)==FALSE)
+
+test_data <- as.data.frame(test_data)
+
+
+# Set up data frame
+
+setup_vars <- data.frame()
+
+for(i in 1:length(test_data$School)){
+  for(j in 1:length(test_data$School)){
+    x <- filter(test_data, School == test_data[i,1])
+    y <- filter(test_data, School == test_data[j,1]) 
+    new <- data.frame(x,y)
+    setup_vars <- rbind(setup_vars,new)
+  }
+}
+
+prediction_data <- setup_vars %>% filter(School != School.1)
+
+#write.csv(prediction_data,"C:/Users/rober//Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/Processed_data/prediction_data.csv")
+
+#Predict class probabilities (i.e. 'certainty' scores)
+
+prediction_data_sub <- prediction_data %>% 
+  filter(season ==2017) %>% select(-season, -School, -season.1,   -School.1)
+pred <- predict(model,prediction_data_sub,"prob")
+head(pred)
+
+prediction_data$lteam  <- pred[,1]
+prediction_data$wteam  <- pred[,2]
+
+
+output_predictions <- prediction_data %>% select(School,School.1,wteam,lteam)
+
+#write.csv(output_predictions, "C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2018/model_predictions_2018.csv")
+
+
+
+# secret sauce  (ahhhh)----------------------
+clean_pred <- data.frame()
+for(i in unique(output_predictions$School)){ 
+  for(j in unique(output_predictions$School)){
+    if(i != j){
+      t1 <- as.numeric(output_predictions %>% filter(School==i & School.1==j) %>% select(wteam))
+      t2 <- as.numeric(output_predictions %>% filter(School==j & School.1==i) %>% select(wteam))
+      
+      if(t1 > t2){
+        num <- t1-t2 
+        df <- data.frame(i,j,i,num*100) 
+        colnames(df) <- c("Team1","Team2","predicted_winner","probability_difference")
+        clean_pred <- rbind(clean_pred,df)
+      } else {
+        num <- t2-t1
+        df <- data.frame(i,j,j,num*100)
+        colnames(df) <- c("Team1","Team2","predicted_winner","probability_difference")
+        clean_pred <- rbind(clean_pred,df)
+      }
+    }
+  } 
+} 
+
+clean_pred_sub <- clean_pred %>% filter(Team1 != Team2)
+
+
+# -------------
+#
+tourn_2017 <- read_csv("C:/Users/rober//Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/renamed_all_tournament_wins_1996-2017.csv")
+tourn_2017 <- tourn_2017 %>% select(Team1 = team1, Team2 = team2, winner)
+
+#pred_test_2017 <- read.csv("C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2018/clean_rf_model_predictions_2018.csv")
+
+pred_test_match_2017 <- left_join(tourn_2017,clean_pred_sub)  
+
+prop.table(table(pred_test_match_2017$winner==pred_test_match_2017$predicted_winner))
+
+
+# Predictions 2018 -----------------------------------------
+
+
+test_data_2018 <- read.csv("C:/Users/rober//Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/Processed_data/joined_data.csv")
+test_data_2018 <- test_data_2018 %>% filter(season == "2018")
+
+#teams_2016 <- read.csv("C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/teams_2016_new.csv")
+#teams_2016 <- teams_2016 %>% filter(season2016 == 1) 
+
+
+wins <- read_csv("C:/Users/rober//Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/renamed_all_tournament_wins_1996-2018.csv")
+teams2018 <- wins %>% filter(season==2018) 
+teams2018 <- c(teams2018$team1,teams2018$team2)
+teams2018 <- data.frame(School = unique(teams2018)) 
+teams2018$School <- as.character(teams2018$School)
+teams2018 <- teams2018 %>% arrange(School)
+
+meh <- inner_join(test_data_2018,teams2018) 
+test_data_2018 <- meh  %>% filter(is.na(seed)==FALSE)
+test_data_2018 <- as.data.frame(test_data_2018)
+
+
+# Set up data fram
+
+setup_vars <- data.frame()
+
+for(i in 1:length(test_data_2018$School)){
+  for(j in 1:length(test_data$School)){
+    x <- filter(test_data_2018, School == test_data_2018[i,1])
+    y <- filter(test_data_2018, School == test_data_2018[j,1]) 
+    new <- data.frame(x,y)
+    setup_vars <- rbind(setup_vars,new)
+  }
+}
+
+prediction_data <- setup_vars %>% filter(School != School.1)
+
+write.csv(prediction_data,"C:/Users/rober//Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/Processed_data/prediction_data.csv")
+
+#Predict class probabilities (i.e. 'certainty' scores)
+
+prediction_data_sub <- prediction_data %>% 
+  filter(season ==2016) %>% select(-season, -School, -season.1,   -School.1)
+pred <- predict(model,prediction_data_sub,"prob")
+head(pred)
+
+prediction_data$lteam  <- pred[,1]
+prediction_data$wteam  <- pred[,2]
+
+
+output_predictions <- prediction_data %>% select(School,School.1,wteam,lteam)
+
+#write.csv(output_predictions, "C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2018/model_predictions_2018.csv")
+
+
+
+# secret sauce  (ahhhh)-----------
+clean_pred <- data.frame()
+for(i in unique(output_predictions$School)){ 
+  for(j in unique(output_predictions$School)){
+    if(i != j){
+      t1 <- as.numeric(output_predictions %>% filter(School==i & School.1==j) %>% select(wteam))
+      t2 <- as.numeric(output_predictions %>% filter(School==j & School.1==i) %>% select(wteam))
+      
+      if(t1 > t2){
+        num <- t1-t2 
+        df <- data.frame(i,j,i,num*100) 
+        colnames(df) <- c("Team1","Team2","predicted_winner","probability_difference")
+        clean_pred <- rbind(clean_pred,df)
+      } else {
+        num <- t2-t1
+        df <- data.frame(i,j,j,num*100)
+        colnames(df) <- c("Team1","Team2","predicted_winner","probability_difference")
+        clean_pred <- rbind(clean_pred,df)
+      }
+    }
+  } 
+} 
+
+clean_pred_sub <- clean_pred %>% filter(Team1 != Team2)
+
+
+# -------------
+#
+tourn_2018 <- read_csv("C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2019/renamed_all_tournament_wins_1996-2018.csv")
+tourn_2018 <- tourn_2018 %>% select(Team1 = team1, Team2 = team2, winner)
+
+#pred_test_2016 <- read.csv("C:/Users/rober/Google Drive/Code_scripts/R/forthefuckofit/March_madness_model_2018/clean_rf_model_predictions_2018.csv")
+
+pred_test_match_2018 <- left_join(tourn_2018,clean_pred_sub)  
+
+prop.table(table(pred_test_match_2018$winner==pred_test_match_2018$predicted_winner))
+
+
+crap <- pred_test_match_2018 %>% filter(is.na(predicted_winner) == FALSE)
+prop.table(table(crap$winner==crap$predicted_winner))
